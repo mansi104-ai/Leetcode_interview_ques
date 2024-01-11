c972bc7c9aecd696ff98a025626ccec0d2672acc
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2024-01-11 18:33:34 +0530

    palindrome and 2 sum problems completed

---
+++ Interview_Easy/two_sum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>m;
+        for(int i=0; i<n;i++){
+            int x= nums[i];
+            int y= target-x;
+            if (m.count(y)){
+                return{m[y],i};
+
+            }
+            m[x]=i;
+
+        }           
+
+        }
+        
+    }
