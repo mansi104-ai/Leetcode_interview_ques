0862d32e621a2a74b1a9de00e1afab6fe7439334
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2024-01-14 15:40:56 +0530

    add 2 integers problem done

---
+++ Interview_Easy/Add_2_integers.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        int summ;
+        if(num1,num2>=-100 && num1,num2<=100){
+            summ = num1+num2;
+        }
+        return summ;
+    }
+};
