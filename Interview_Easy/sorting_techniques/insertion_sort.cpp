66c793800f5cd879ea1f66490bda671d511a4aef
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2024-03-27 07:53:34 +0530

    insertion sort part completed

--- Interview_Easy/sorting_techniques/insertion_sort.cpp
+++ Interview_Easy/sorting_techniques/insertion_sort.cpp
@@ -5,21 +5,21 @@ using namespace std;
 class Solution{
 public:
     void swapElements(vector<int>& arr, int n) {
-        for (int i = 0; i < n - 1; ++i) {
-            for (int j = 0; j < n - i - 1; ++j) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+        for (int i = 1; i < n; ++i) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
             }
         }
     }
 };
 
 int main() {
-    vector<int> arr = {1, 3, 55, 23, 12, 24};
-    int n = arr.size();
+    vector<int> arr = {13, 2, 3, 4, 565};
+    int n = sizeof(arr)/sizeof(arr[0]);
     
     Solution s;
     s.swapElements(arr, n);
