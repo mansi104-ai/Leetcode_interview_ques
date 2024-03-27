ba49481e4b29bab6acbeb2b48fe0a5aa8e893625
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2024-03-27 19:33:33 +0530

    quick sort to be completed

---
+++ Interview_Easy/sorting_techniques/quick_sort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void partition(){
+
+}
+
+void sort(vector<int> &arr, int low,int high){
+    for(int i=0;i<arr.size();i++){
+        int low = arr[0];
+        int high = arr.size();
+        
+    }
+
+}
+
+int main(){
+
+}
