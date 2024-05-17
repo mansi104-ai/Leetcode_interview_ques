270c3111ccf493f535de79466a48a9340c958362
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2024-05-17 13:40:37 +0530

    greedy algorithm basic questions with hard to be practiced

---
+++ Concepts/inheritance.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class AbstractEmployee {
+public:
+    virtual void askForPromotion() = 0;
+};
+
+class Employee {
+private:
+    string company;
+    int age;
+
+protected: 
+    string name;
+
+public:
+    Employee(string name, string company, int age) {
+        this->name = name;
+        this->company = company;
+        this->age = age;
+    }
+
+    void printInfo() {
+        cout << "The name of the employee is: " << name << ", works in company: " << company << ", with age: " << age << endl;
+    }
+
+    void setName(string name) {
+        this->name = name;
+    }
+
+    string getName() {
+        return name;
+    }
+
+    void setAge(int age) {
+        this->age = age;
+    }
+
+    int getAge() {
+        return age;
+    }
+};
+
+class Developer : public Employee {
+public:
+    string favProgrammingLanguage;
+
+    Developer(string name, string company, int age, string favProgrammingLanguage) : Employee(name, company, age) {
+        this->favProgrammingLanguage = favProgrammingLanguage;
+    }
+
+    void fixBug() {
+        cout << name << " fixed the bug using: " << favProgrammingLanguage << "\n";
+    }
+};
+
+int main() {
+    Developer d = Developer("John", "Apple", 30, "C++");
+    d.fixBug();
+    return 0;
+}
---
+++ Interview_Easy/greedy/max_sum_k_neg.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution:
+    public{
+        int largestSumAfterKNegations(vector<int>& nums , int k){
+            ranges::sort(nums);
+
+            for(int i=0; i<nums.size();i++){
+                if(nums[i]>0 || k == 0){
+                    break;
+                }
+                nums[i] == -nums[i];
+                --k;
+            }
+
+            return accumulate(nums.begin(),nums.end(),0)- ( k % 2 )*ranges::min(nums) * 2;
+        }
+    };
