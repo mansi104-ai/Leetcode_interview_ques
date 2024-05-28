ab56d86956d638ee1fd62fd17ec2764c140858d0
mansi104-ai <mkb.kalra@gmail.com>
2024-05-28 10:11:09 +0530

    Leetcode ques to be completed till today

--- Leetcode_contests/Contest_399/Find_good_pairsII.java
+++ Leetcode_contests/Contest_399/Find_good_pairsII.java
@@ -1,19 +1,25 @@
-class Solution{
-    public long  numberOfPairs(int[] nums1, int[] nums2, int k){
-        Map<integer,integer> map = new HashMap<>();
-        long pairs =0;
-        for(int i:nums2){
-            map.put(i, getOrDefault(i,0)+1);
-        }
+class Solution {
+    public long numberOfPairs(int[] nums1, int[] nums2, int k) {
+        Map<Integer, Integer> map =  new HashMap<>();
+        long pairs =0 ;
+        for (int i:nums2){
+            map.put(i,map.getOrDefault(i,0)+1);
 
+        }
         for(int i:nums1){
-            if(i%k ==0){
+            if(i% k ==0){
                 int temp = i/k;
-                for(int j=0; j*j <=temp;j++){
-                    
+                for(int j=1; j*j<=temp; j++){
+                    if(temp %j == 0){
+                        pairs += (long)map.getOrDefault(j,0);
+                        if(j != temp/j){
+                            pairs += (long)map.getOrDefault(temp/j,0);
+                        }
+                    }
                 }
             }
         }
-
+        return pairs;
+        
     }
 }
