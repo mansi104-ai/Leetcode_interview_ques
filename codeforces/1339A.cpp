63c5305a04a1f5f66efe708acec54fc7d024c578
mansi104-ai <mkb.kalra@gmail.com>
2025-05-15 17:42:46 +0530

    problems completed for 4/7

--- codeforces/1339A.cpp
+++ codeforces/1339A.cpp
@@ -3,5 +3,12 @@
 #include <cmath>
 using namespace std;
 int main(){
-    
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << n << endl;// hahahaha 
+    }
+    return 0;
 }
