6e81bbd4357655503a81f787373d5d7084b55bd6
mansi104-ai <mkb.kalra@gmail.com>
2025-07-31 19:30:45 +0530

    bst to be completed

--- Graphs/bfs_implementation.cpp
+++ Graphs/bfs_implementation.cpp
@@ -9,14 +9,10 @@ public:
         // int n = adj.size();
 
         vector<int> vis(V,0);
-
-        queue<int> q;
         vis[0] = 1;
-
+        queue<int> q;
         q.push(0);
-
         vector<int> bfs;
-
         while(!q.empty()){
             int node = q.front();
             q.pop();
@@ -24,12 +20,12 @@ public:
 
             for(auto it: adj[node]){
                 if(!vis[it]){
+                    vis[it] = 1;
                     q.push(it);
-                    vis[it] =1;
                 }
             }
-
         }
+
         return bfs;
 
     }
